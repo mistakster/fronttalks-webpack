2e5aa6188ad38b9a80d28eea2161894d65239525
Vladimir Kuznetsov <vladimir.kuznetsov@engine6.com>
2015-09-13 14:30:29 +0500

    implemented video player

---
+++ assets/video-player.js
@@ -0,0 +1,39 @@
+(function () {
+
+	function log(text) {
+		var logger = document.getElementById('log');
+		var para = document.createElement('pre');
+		para.innerHTML = text;
+		logger.appendChild(para);
+	}
+
+	var players = document.querySelectorAll('.video-player');
+
+	function initPlayer(playerContainer) {
+		var video = playerContainer.querySelector('video');
+		var observer = new MutationObserver(function (mutations) {
+			mutations.forEach(function (record) {
+				if (record.attributeName == 'class') {
+					if (playerContainer.classList.contains('active')) {
+						if (video.paused) {
+							video.currentTime = 0;
+							video.play();
+						}
+					} else {
+						if (!video.paused) {
+							video.pause();
+						}
+					}
+				}
+			});
+		});
+		observer.observe(playerContainer, {
+			attributes: true
+		});
+	}
+
+	for (var i = 0; i < players.length; i++) {
+		initPlayer(players[i]);
+	}
+
+})();
