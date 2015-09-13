7f64abd1702975a16f08204bdc60a47383d7fc59
Vladimir Kuznetsov <vladimir.kuznetsov@engine6.com>
2015-09-13 16:17:51 +0500

    hook only on class change events

--- assets/video-player.js
+++ assets/video-player.js
@@ -1,38 +1,29 @@
 (function () {
 
-	function log(text) {
-		var logger = document.getElementById('log');
-		var para = document.createElement('pre');
-		para.innerHTML = text;
-		logger.appendChild(para);
-	}
-
-	var players = document.querySelectorAll('.video-player');
-
 	function initPlayer(playerContainer) {
 		var video = playerContainer.querySelector('video');
 		var observer = new MutationObserver(function (mutations) {
 			mutations.forEach(function (record) {
-				if (record.attributeName == 'class') {
-					if (playerContainer.classList.contains('active')) {
-						if (video.paused) {
-							video.currentTime = 0;
-							video.play();
-						}
-					} else {
-						if (!video.paused) {
-							video.pause();
-							video.currentTime = 0;
-						}
+				if (playerContainer.classList.contains('active')) {
+					if (video.paused) {
+						video.currentTime = 0;
+						video.play();
+					}
+				} else {
+					if (!video.paused) {
+						video.pause();
+						video.currentTime = 0;
 					}
 				}
 			});
 		});
 		observer.observe(playerContainer, {
-			attributes: true
+			attributes: true,
+			attributeFilter: ['class']
 		});
 	}
 
+	var players = document.querySelectorAll('.video-player');
 	for (var i = 0; i < players.length; i++) {
 		initPlayer(players[i]);
 	}
